8cfa7f6e2c3f8a20fb386f97912ca729c1835a71
Alzubair98 <203721133@ogr.altinbas.edu.tr>
2023-12-21 01:34:56 +0300

    create reuseable component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,12 @@
-import Image from "next/image";
+import Hero from "@/components/hero";
 import home from "../../public/home.jpg";
 
 export default function Home() {
   return (
-    <div>
-      Home Page
-      <div className="absolute -z-10 inset-0">
-        <Image
-          alt="car factory"
-          src={home}
-          fill
-          style={{ objectFit: "cover" }}
-        />
-      </div>
-    </div>
+    <Hero
+      imgData={home}
+      imgAlt="car factory"
+      title="Professional Cloud hosting"
+    />
   );
 }
