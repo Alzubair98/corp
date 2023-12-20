eaf25db05d7eb659b646ade01adda592947bbcd2
Alzubair98 <203721133@ogr.altinbas.edu.tr>
2023-12-21 00:51:08 +0300

    add image to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,18 @@
+import Image from "next/image";
+import home from "../../public/home.jpg";
+
 export default function Home() {
-  return <div>Home Page</div>;
+  return (
+    <div>
+      Home Page
+      <div className="absolute -z-10 inset-0">
+        <Image
+          alt="car factory"
+          src={home}
+          fill
+          style={{ objectFit: "cover" }}
+        />
+      </div>
+    </div>
+  );
 }
