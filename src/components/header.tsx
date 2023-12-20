0c8a15c6548c51b6c81c9f944eef61e8386a184c
Alzubair98 <203721133@ogr.altinbas.edu.tr>
2023-12-21 01:45:52 +0300

    fix style

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -4,10 +4,14 @@ export default function Header() {
   return (
     <div className="w-full absolute text-white z-10">
       <nav className="container relative flex flex-wrap items-center justify-between mx-auto p-8">
-        <Link href="/">Home</Link>
-        <Link href="/performance">Performace</Link>
-        <Link href="/reliability ">Reliablility</Link>
-        <Link href="/scale">Scale</Link>
+        <Link href="/" className="font-bond text-3xl">
+          Home
+        </Link>
+        <div className="space-x-4 text-xl">
+          <Link href="/performance">Performace</Link>
+          <Link href="/reliability ">Reliablility</Link>
+          <Link href="/scale">Scale</Link>
+        </div>
       </nav>
     </div>
   );
